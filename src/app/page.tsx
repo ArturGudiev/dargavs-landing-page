337fefe65dc599e60eeee5d70ad0580189fc8566
Artur Gudiev <arturgudiev>
2026-08-06 00:06:44 -0700

    add stlyes to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,50 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-[1fr_auto_1fr] w-full  pt-[60px] pb-[60px]">
-      {/* Левая часть */}
-      <div className="text-left pl-[100px]">
-        <Image src="/logotype.png" alt="Logotype" width={130} height={48} priority />
-      </div>
+        {/* Левая часть */}
+        <div className="text-left pl-[100px]">
+          <Image src="/logotype.png" alt="Logotype" width={130} height={48} priority />
+        </div>
 
-      {/* Центр (займет ровно столько, сколько нужно контенту) */}
-      <div className="pt-[57px] flex gap-4 text-[#3D6CB9]">
-        <div>Главная</div>
-        <div>Меню</div>
-        <div>О нас</div>
-        <div>Отзывы</div>
-        <div>Контакты</div>
-      </div>
+        {/* Центр (займет ровно столько, сколько нужно контенту) */}
+        <div 
+          className="pt-[57px] flex gap-4 text-[#3D6CB9]"
+          style={{ letterSpacing: '1.5px' }}  
+        >
+          <div className={styles.navLink}>Главная</div>
+          <div className={styles.navLink}>Меню</div>
+          <div className={styles.navLink}>О нас</div>
+          <div className={styles.navLink}>Отзывы</div>
+          <div className={styles.navLink}>Контакты</div>
+        </div>
 
-      {/* Правая часть */}
-      <div className="text-right pr-[100px] pt-[30px]">
-        <div style={{ width: '80px', height: '80px', borderColor: 'red', borderWidth: '1px', borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', 
-          position: 'absolute',  
-          right: '120px',
-          
-          }}>
-          <Image
-            src="/heart.svg"
-            className="w-[34px] h-[34px] border border-red-500 "
+        {/* Правая часть */}
+        <div className="text-right pr-[100px] pt-[30px]">
+          <div style={{ 
+            width: '80px', 
+            height: '80px',
+            backgroundColor: '#FFFFFF',
+            borderRadius: '50%',
+            boxShadow: '0px 2px 16px 0px rgba(168, 200, 254, 0.25)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center', 
+            position: 'absolute',  
+            right: '120px',          
+            }}>
+            <Image
+              src="/heart.svg"
+              className="w-[34px] h-[34px]"
               alt="like" 
               width={40} 
               height={34} 
               priority 
             />
-        </div>
+          </div>
 
         </div>
       </div>
@@ -44,22 +55,43 @@ export default function Home() {
            style={{ fontFamily: "Cormorant", color: "#8DB2FE" }}
             className="pt-[115px] font-bold italic text-[28px] leading-[100%] tracking-[0px]"
           >Добро пожаловать в</div>
-          <div style={{ fontFamily: "Cormorant", color: "#6B7FA7" }}
-            className="pt-[17px] font-bold text-[50px] leading-[100%] tracking-[0px]"
-            >Кафе под открытым небом</div>
-          <div style={{ fontFamily: "Antarctic", color: "#FFD683" }}
-            className="pt-[17px] text-[70px] font-normal leading-[68px] tracking-[0.16em]"
-          >dargavs cafe</div>
-          <div>здесь время останавливается</div>
-
-          <div>
-          Наслаждайтесь вкусной едой и красивым видом в нашем кафе с видом на горы. 
-          Окунитесь в мир вкуса и красоты! 
-          Наше кафе в горах Северной Осетии — это не просто еда,              
-          это целое приключение. Мы готовим из местных продуктов,                            
-          чтобы каждый кусочек был свежим и незабываемым.
+          
+          <div className="pt-[17px] relative h-[85px]">
+            <div 
+              style={{ 
+                fontFamily: "Cormorant", 
+                color: "#6B7FA7",
+                position: 'absolute',
+              }}
+              className="font-bold text-[50px] leading-[100%] tracking-[0px]"
+              >
+                Кафе под открытым небом
+              </div>
+              <div 
+                className="absolute pl-[70px] pt-[17px] font-['Antarctic'] text-[70px] font-normal text-[#FFD683] leading-[68px] tracking-[0.16em]"
+              >dargavs cafe</div>
+            </div>
+         
+          <div
+            className="pt-[17px] text-[28px] ml-auto mr-[1rem] text-right font-['Cormorant'] italic text-[#8DB2FE] font-bold"
+          >
+            здесь время останавливается
           </div>
-          <button>Позвонить</button>
+
+          <div
+            className="mt-[50px] text-[16px] font-medium leading-[1.2] text-[#6B7FA7] space-y-[20px]"
+          >
+            <p>
+              Наслаждайтесь вкусной едой и красивым видом в нашем кафе с видом на горы.
+              Окунитесь в мир вкуса и красоты!
+            </p>
+            <p>
+              Наше кафе в горах Северной Осетии — это не просто еда,
+              это целое приключение. Мы готовим из местных продуктов,
+              чтобы каждый кусочек был свежим и незабываемым.
+            </p>
+          </div>
+          {/* <button>Позвонить</button> */}
         </div>
         <div className="border w-[818px]">Image gallery carusel</div>
       </div>
